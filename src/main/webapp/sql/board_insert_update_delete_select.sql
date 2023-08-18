ee210933ed157dfc988aea38452bce06d1d84626
WooHanYeong <woocoding96@gmail.com>
2023-08-18 19:49:17 +0900

    ZZ

--- src/main/webapp/sql/board_insert_update_delete_select.sql
+++ src/main/webapp/sql/board_insert_update_delete_select.sql
@@ -1,3 +1,5 @@
+--insert
+
 INSERT INTO BOARD (BOARD_NO, BOARD_TITLE, BOARD_CONTENT, BOARD_DATE, BOARD_ID)
 VALUES (1, '그만먹고싶어요', '너무맛있어서 그만먹고싶어요', SYSDATE, 'WHY');
 
@@ -17,6 +19,28 @@ INSERT INTO BOARD (BOARD_NO, BOARD_TITLE, BOARD_CONTENT, BOARD_DATE, BOARD_ID)
 VALUES (6, '환불 요청', '제품 불만족으로 환불을 신청합니다.', SYSDATE, 'customer3');
 
 INSERT INTO BOARD (BOARD_NO, BOARD_TITLE, BOARD_CONTENT, BOARD_DATE, BOARD_ID)
-VALUES (7, '교환 요청', '제품 교환이 필요해서 신청합니다.', SYSDATE, 'customer3');
+VALUES (7, '교환 요청', '제품 교환이 필요해서 신청합니다.', SYSDATE, 'customer4');
 
+INSERT INTO BOARD (BOARD_NO, BOARD_TITLE, BOARD_CONTENT, BOARD_DATE, BOARD_ID)
+VALUES (8, '추가구매', '한개 더 시키면 언제오나요??', SYSDATE, 'customer5');
 
+INSERT INTO BOARD (BOARD_NO, BOARD_TITLE, BOARD_CONTENT, BOARD_DATE, BOARD_ID)
+VALUES (9, '감사합니다', '아이유보고 암이 사라졌어요!!', SYSDATE, 'customer6');
+
+INSERT INTO BOARD (BOARD_NO, BOARD_TITLE, BOARD_CONTENT, BOARD_DATE, BOARD_ID)
+VALUES (10, '정말최악', '피자스쿨이 더 맛있어요 ㅋㅋ', SYSDATE, 'customer7');
+
+INSERT INTO BOARD (BOARD_NO, BOARD_TITLE, BOARD_CONTENT, BOARD_DATE, BOARD_ID)
+VALUES (11, '삭제용', '삭제용', SYSDATE, '삭제용');
+
+--update
+
+UPDATE board
+SET  board_content='수정수정2', board_title='수정했당22'
+WHERE BOARD_NO=11;
+
+--delete
+DELETE board WHERE board_no=11;
+
+--select (1)
+SELECT * FROM BOARD WHERE board.board_no=1;
