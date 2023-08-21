60ad4adbdeb5bb7b8efd507e6bbfa70b1013bc0d
ggwanghun <ggwanghun@gmail.com>
2023-08-21 18:23:35 +0900

    ...

--- src/main/java/com/itwill/pizza/orders/OrdersDaoImplMyBatis.java
+++ src/main/java/com/itwill/pizza/orders/OrdersDaoImplMyBatis.java
@@ -1,5 +1,7 @@
 package com.itwill.pizza.orders;
 
+import java.util.List;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,24 +12,24 @@ import com.itwill.pizza.mapper.OrderMapper;
 
 
 
-//public class OrdersDaoImplMyBatis implements OrdersDao {
-//
-//	private SqlSessionFactosry sqlSessionFactory;
-//
-//	public OrdersDaoImplMyBatis() throws Exception {
-//		this.sqlSessionFactory = new SqlSessionFactoryBuilder()
-//				.build(Resources.getResourceAsStream("mybatis-config.xml"));
-//	}
-//
-//
-//	@Override
-//	public int insertOrder(Order order) throws Exception {
-//		SqlSession sqlSession= sqlSessionFactory.openSession(true);
-//		OrderMapper orderMapper=sqlSession.getMapper(OrderMapper.class);
-//		int rowCount=orderMapper.insertOrder(order);
-//		sqlSession.close();
-//		return rowCount;
-//	}
+public class OrdersDaoImplMyBatis implements OrdersDao {
+
+	private SqlSessionFactory sqlSessionFactory;
+
+	public OrdersDaoImplMyBatis() throws Exception {
+		this.sqlSessionFactory = new SqlSessionFactoryBuilder()
+				.build(Resources.getResourceAsStream("mybatis-config.xml"));
+	}
+
+
+	@Override
+	public int insertOrder(Order order) throws Exception {
+		SqlSession sqlSession= sqlSessionFactory.openSession(true);
+		OrderMapper orderMapper=sqlSession.getMapper(OrderMapper.class);
+		int rowCount=orderMapper.insert(order);
+		sqlSession.close();
+		return rowCount;
+	}
 
 	
-//}
+}
