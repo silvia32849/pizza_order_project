ce214dac6b13c74a5af9d55c04a164c2bdd54fc9
hungrydog3366 <hungrydog3366@gmail.com>
2023-08-21 10:23:03 +0900

    ..

--- src/main/resource/ERD/userinfo_insert_update_delete_select.sql
+++ src/main/resource/ERD/userinfo_insert_update_delete_select.sql
@@ -2,11 +2,11 @@
 INSERT INTO userinfo(user_id, user_pw, user_name, user_address, user_email, user_jumin, user_phone, user_gender)
 VALUES('user1', '1234', '박명수', '서울시', 'email@naver.com', '990101-1234567', '010-1234-5678', '남');
 INSERT INTO userinfo(user_id, user_pw, user_name, user_address, user_email, user_jumin, user_phone, user_gender)
-VALUES('user2', '2345', '김유신', '서울시', 'email2@naver.com', '940202-2345678', '010-2345-6789', '남');
+VALUES('user2', '2345', '김유신', '서울시', 'email2@naver.com', '940202-1345678', '010-2345-6789', '남');
 INSERT INTO userinfo(user_id, user_pw, user_name, user_address, user_email, user_jumin, user_phone, user_gender)
-VALUES('user3', '3456', '임정희', '서울시', 'email3@naver.com', '920303-3456789', '010-3456-7890', '여');
+VALUES('user3', '3456', '임정희', '서울시', 'email3@naver.com', '920303-2456789', '010-3456-7890', '여');
 INSERT INTO userinfo(user_id, user_pw, user_name, user_address, user_email, user_jumin, user_phone, user_gender)
-VALUES('user4', '4567', '권희수', '서울시', 'email4@naver.com', '930404-4567890', '010-4567-8901', '여');
+VALUES('user4', '4567', '권희수', '서울시', 'email4@naver.com', '930404-2567890', '010-4567-8901', '여');
 
 --update
 UPDATE userinfo
